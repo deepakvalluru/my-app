f75fcd4f1f222276456cf64fd9cc8ef842ed3d30
Unknown <deepakvalluru@gmail.com>
2017-03-25 01:39:44 -0500

    Heroes component added.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-component',
+  templateUrl: './course.component.html'
+})
+export class CourseComponent {
+  title = 'This is course component';
+}
